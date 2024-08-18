8091e653ec07097613b982b431dec02d3b59cf8e
AmmarFK <farooqammar51@yahoo.com>
2024-08-18 22:07:14 +1000

    changes

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("postgresql://task4p_4ocs_user:RNomcRh19vUpP4UGRWKqYen8oHpYQmXw@dpg-cr0qspbqf0us73fearn0-a.oregon-postgres.render.com/task4p_4ocs")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
